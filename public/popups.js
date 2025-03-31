a58135b36419fe1a90c1124ce585163f3aa4e819
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-03-31 18:00:44 +0200

    add icon on all font

--- public/popups.js
+++ public/popups.js
@@ -30,6 +30,8 @@ const popups = [
     `Comment ça tu n'est pas un tipeur !?? <br><button><a href="https://tipeee.com/silverdium">TIPEEE</a></button>`,
 
     `Le serveur Minecraft Silverdium était tous d'abord<br>un serveur Discord entre amis !`,
+
+    `SilverCore lutte pour le respect de la vie privée !`,
     
     '<a style="color: black; text-decoration: none;" href="https://core.silverdium.fr">SilverCore</a> est <strong>gratuit</strong> pour cela <a href="https://tipeee.com/silverdium">tu peux nous aider</a> !'
 
