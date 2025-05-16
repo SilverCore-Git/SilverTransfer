125d6aa393a6768b8bd86ac02f1c8ac7fc22acde
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-05-16 16:56:48 +0200

    add archive stats function

--- src/stats_manager.js
+++ src/stats_manager.js
@@ -1,11 +1,15 @@
+
 const fs = require('fs');
 const path = require('path');
+
 const statsPath = path.join(__dirname, '../db/stats.json');
+const archive_dir = path.join(__dirname, '../db/archives');
+const stats_archive_dir = path.join(__dirname, '../db/archives/stats');
 
 class stats {
     
-    // Charge ou initialise la DB
     load() {
+
         if (!fs.existsSync(statsPath)) {
             return {
                 stats_db: {
@@ -27,7 +31,31 @@ class stats {
                 }
             };
         }
+
         return JSON.parse(fs.readFileSync(statsPath));
+    }
+
+    archive(data) {
+
+        try {
+
+            if (!fs.existsSync(archive_dir)) fs.mkdir(archive_dir);
+            if (!fs.existsSync(stats_archive_dir)) fs.mkdir(stats_archive_dir);
+
+            const stats_archive_file = `stats_archive_${new Date()}_silvertransfert.json`;
+
+            fs.writeFileSync(path.join(stats_archive_dir + '/' + stats_archive_file), JSON.stringify(data, null, 2));
+
+            fs.unlink(statsPath);
+
+            this.save(this.load());
+
+        }
+
+        catch (err) {
+            return console.error('Une erreur est survenue lors de l\'archivage des stats : ', err );
+        };
+
     }
 
     save(data) {
