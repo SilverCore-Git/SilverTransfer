294f28ee1012d481761884167fe9e90e2ba756e1
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-11-28 16:25:06 +0100

    rewright upload route

--- src/routes/upload/afterUpload.ts
+++ src/routes/upload/afterUpload.ts
@@ -1,22 +1,20 @@
 import key from "../../assets/Crypter/key_manager";
 import encryptFile from "../../assets/Crypter/EncryptFile";
-import path from "path";
-import type { Request } from "express";
 import db from "../../assets/database/db";
 
 export default async function
-({ transferID, req }: { transferID: string, req: Request })
+(
+    { 
+        transferID, 
+        tempFilePath, 
+        encryptedFilePath
+    }: 
+    { 
+        transferID: string, 
+        tempFilePath: string, 
+        encryptedFilePath: string
+    })
 {
-
-    if (!req.file) return;
-
-    const { passwd } = req.body;
-
-    // générate crypter key + define const
-    await key.generate(transferID, passwd);
-    const tempFilePath = req.file.path;
-    const encryptedFileName = `${transferID}.${req.file.filename}.enc`;
-    const encryptedFilePath = path.join(__dirname, `../${config.DATAdir}`, encryptedFileName);
 
     try {
 
@@ -34,7 +32,7 @@ export default async function
         const transfer = await db.get(transferID);
         if (!transfer) return;
 
-        transfer.statu = 'ready';
+        transfer.status = 'ready';
         
         await db.update(transfer);
 
---
+++ src/routes/upload/upload.ts
@@ -0,0 +1,88 @@
+// packages
+import { Router } from "express";
+const router = Router();
+import multer from "multer";
+import path from 'path';
+import text_crypter from "../../assets/Crypter/Text_crypter";
+import db from "../../assets/database/db";
+import key from "../../assets/Crypter/key_manager";
+import getClientIp from "../../assets/utils/getClientIp";
+import { getCurrentDate, getCurrentTime } from "../../assets/utils/getDate";
+import { Transfert } from "../../assets/database/dbTypes";
+import afterUpload from "./afterUpload";
+
+const uploadDir = path.join(__dirname, '../', config.TEMPdir);
+
+// Configuration de Multer pour stocker les fichiers sur disque
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
+        const encryptedText = text_crypter.encrypt(file.originalname);
+        const fileExt = path.extname(file.originalname);
+        const newFileName = `${encryptedText}${fileExt}`;
+        cb(null, newFileName);
+    }
+});
+
+const upload = multer({ 
+    storage,
+    limits: {
+        fileSize: 16 * 1024 * 1024 * 1024 // 16 Go
+    }
+ });
+
+
+
+
+router.get('/file', async (req, res) => {
+    res.send('ok')
+})
+
+router.post('/file', upload.single("file"), async (req, res) => {
+
+    const { id, passwd }: { id: string, passwd: string } = req.body;
+    const ip: string = getClientIp(req);
+
+    if (!req.file) {
+        return res.status(400).json({ message: "Aucun fichier reçu" });
+    }
+
+    console.log('New transfer : ', req.body.id);
+
+    await key.generate(id, passwd);
+    const tempFilePath = req.file.path;
+    const encryptedFileName = `${id}.${req.file.filename}.enc`;
+    const encryptedFilePath = path.join(__dirname, `../${config.DATAdir}`, encryptedFileName);
+    const downloadPath: string = `https://t.silvertransfert.fr/${id}-${passwd}`;
+
+    const transfer: Transfert = {
+        UUID: req.body.id as string,
+        cryptedFileName: encryptedFileName,
+        tempFileName: path.basename(tempFilePath),
+        size: req.file.size as number,
+        senderIp: ip,
+        date: `${getCurrentDate()} - ${getCurrentTime()}`,
+        status: 'await_crypting'
+    }
+
+    await db.push(transfer);
+
+    res.json({
+        status: transfer.status,
+        message: "Fichier reçu !",
+        id,
+        downloadPath
+    });
+
+    await afterUpload({
+        transferID: id,
+        encryptedFilePath,
+        tempFilePath
+    });
+
+});
+
+
+export default router;
