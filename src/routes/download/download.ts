2c49ed4959c4de5c4e850b867af6f55a4ce45b31
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-11-28 20:36:04 +0100

    create decrypte file func

--- src/routes/download/download.ts
+++ src/routes/download/download.ts
@@ -5,6 +5,7 @@ import path from 'path';
 import fs from 'fs';
 import db from "../../assets/database/db";
 import { Transfert } from "../../assets/database/dbTypes";
+import decrypteFile from "./decrypteFile";
 
 
 router.get('/download', (req, res) => {
@@ -15,7 +16,30 @@ router.get('/download', (req, res) => {
 
 })
 
-router.get('/decrypt', (req, res) => {
+router.get('/decrypt', async (req, res) => {
+
+    const { id, passwd }: { id: string, passwd: string } = req.body;
+
+    const transfer = await db.get(id);
+    if (!transfer) return res.status(404).json({ error: true, message: 'transfer not found' });
+
+    if (transfer.status == 'ready_to_download')
+    {
+        return res.json({ ready_to_download: true });
+    }
+
+    const encryptedFilePath = path.join(__dirname, "../data", transfer.cryptedFileName);
+    const decryptedFilePath = path.join(__dirname, "../temp", transfer.tempFileName);
+
+    res.json({ status: 'processing' });
+
+    await decrypteFile({
+        transferID: id,
+        req,
+        encryptedFilePath,
+        decryptedFilePath,
+        passwd
+    });
 
 })
 
