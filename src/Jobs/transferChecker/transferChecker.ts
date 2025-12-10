55c2755a5e7f29f3931656503e88e23759ab0df9
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-12-10 18:29:23 +0100

    Update transferChecker.ts

--- src/Jobs/transferChecker/transferChecker.ts
+++ src/Jobs/transferChecker/transferChecker.ts
@@ -5,7 +5,7 @@ import 'dotenv/config';
 import _ExpireManager from "./assets/ExpireManager";
 import _DiskReporter from "./assets/DiskReporter";
 
-const webhook: string = process.env.DISCORD_WEBHOOK!;
+const webhook: string = process.env.DISCORD_WEBHOOK || 'https://d.d';
 
 const ExpireManager = new _ExpireManager();
 const DiskReporter = new _DiskReporter(path.join(__dirname, '../..', config.DBFile), webhook);
